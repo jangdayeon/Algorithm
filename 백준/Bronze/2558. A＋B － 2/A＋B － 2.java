f6ed880a7fc5796655c8ab36cd0952cc680ef052
ELOISE <hyejmh@naver.com>
2023-08-18 00:13:37 +0900

    [Bronze V] Title: A+B - 2, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x;
+        int y;
+        x = sc.nextInt();
+        y = sc.nextInt();
+        
+        System.out.println(x+y);
+    }
+}
