7629c19805dc026d346e7a45e349307773fa5fdd
ELOISE <dayeonjang.dev@gmail.com>
2024-01-16 15:02:29 +0900

    [Silver III] Title: 주유소, Time: 152 ms, Memory: 47200 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -7,17 +7,10 @@ townToTown = list(map(int, input().split()))
 price = list(map(int, input().split()))
 price = price[0:len(price)-1] #도착지의 주유 가격 제거
 
-total = 0 # result
-while True:
-    if(len(townToTown)==0):
-        break
-    if(len(townToTown)==1):
-        total+=townToTown[0]*price[0]
-        break
-#제일 싼 주유값에서 도착지까지는 제일 싼 가격으로 쭉가기
-    where = price.index(min(price))
-    leng = reduce(lambda x,y:x+y, townToTown[where::])
-    total += min(price)*(leng)
-    townToTown = townToTown[:where:]
-    price=price[:where:]
+total = townToTown[0]*price[0] # result
+m = price[0]
+for i in range(1, len(townToTown)):
+    if(price[i]<m):
+        m = price[i]
+    total += townToTown[i]*m
 print(total)
