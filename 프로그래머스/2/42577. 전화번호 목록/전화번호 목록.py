fe7764c2cffcba56cf4b072ea04fc454be34ceb0
ELOISE <dayeonjang.dev@gmail.com>
2024-02-14 13:34:24 +0900

    [level 2] Title: 전화번호 목록, Time: 102.54 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,3 +1,6 @@
+#string 정렬은 앞의 글자가 작은 수부터 정렬되므로 이 점을 이용해야 함
+#String 정렬과 Int 정렬의 차이점을 꼭 기억할 것
+
 def solution(phone_book):
     phone_book = sorted(phone_book)
     for i in range(1,len(phone_book)):
