032a4d79991bb5abf8d61581d1d468e876c0426d
ELOISE <dayeonjang.dev@gmail.com>
2024-02-15 12:55:37 +0900

    [level 2] Title: 주식가격, Time: 60.16 ms, Memory: 17.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,3 +1,7 @@
+#이것도 아이디어가 ,, ㅎㅎ prices 길이가 10만까지 가서 log2N으로 풀었을 줄 알았는데 n2으로도 가능
+#정석대로 풀었다.
+#첫번째값 이후에 첫번째 값보다 작은 값 발견시 해당 인덱스를 answer에 추가하고 for문을 break하고 다음 인덱스로 넘어가기
+
 def solution(prices):
     answer = []
     for i in range(len(prices)):
