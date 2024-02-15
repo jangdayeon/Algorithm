39def599f513ba0e6ab102ddd6a5389e31d18eeb
ELOISE <dayeonjang.dev@gmail.com>
2024-02-15 12:21:33 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 70.01 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,3 +1,5 @@
+#participant와 completion을 정렬하고 값이 다른 곳의 값을 리턴 (한 사람 밖에 완주하지 못했다고 했기에)
+
 def solution(participant, completion):
     participant = sorted(participant)
     completion = sorted(completion)
