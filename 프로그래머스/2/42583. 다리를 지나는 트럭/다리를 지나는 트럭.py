c24cd5a6a5de556503bfe1cf9f7ed760bd6158c5
ELOISE <dayeonjang.dev@gmail.com>
2024-02-15 12:42:05 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,3 +1,11 @@
+#아이디어 바로 생각이 안났으니 한 번 더!
+
+#bridge의 길이가 0이 될 때까지 while문을 돌려두고
+#기본적으로, 다리를 건너는 트럭에서 하나의 값을 pop하기
+#그 다음에는,,
+#다음 들어올 차가 더해진 무게와 길이가 괜찮으면 대기 트럭에서 빼오고,
+#안 괜찮으면 0을 추가하기
+
 from collections import deque
 def solution(bridge_length, weight, truck_weights):
     bridge = deque([0]* bridge_length)
