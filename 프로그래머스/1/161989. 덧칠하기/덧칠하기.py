48fd1a25f421f51ce296b0249a617fcb702183a0
ELOISE <dayeonjang.dev@gmail.com>
2024-05-02 16:57:11 +0900

    [level 1] Title: 덧칠하기, Time: 2.25 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,13 +1,11 @@
 def solution(n, m, section):
-    li = [True] * n
-    for i in section: #벗겨진 곳 체크
-        li[i-1] = False
     answer = 0
-    for sec in section: 
-        if li[sec-1] == False: #벗겨진 곳 찾고
-            if sec-1+m >= len(li): #벗겨진 곳부터 칠하려고 하는데 벽을 넘어갈 경우
-                li[sec-1:] = [True] *len(li[sec-1:])
-            else: #그냥 칠하면 되는 경우
-                li[sec-1:sec-1+m] = [True] * len(li[sec-1:sec-1+m])
-            answer +=1 #페인트칠 몇 번 했는지 체크
-    return answer  
+
+    h = section[0] #처음 시작
+
+    for v in section: #색을 칠해야 하는 곳
+        if(v - h + 1 > m): #색을 칠해야 하는 곳 - 색 칠하기 완성한 곳+1>롤러의 길이
+            answer += 1
+            h = v
+
+    return answer + 1
