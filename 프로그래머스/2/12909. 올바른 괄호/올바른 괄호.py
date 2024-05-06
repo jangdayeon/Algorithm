95c0c66ed1b225287b21330a9f5f9fdc7b46c146
ELOISE <dayeonjang.dev@gmail.com>
2024-05-06 11:12:53 +0900

    [level 2] Title: 올바른 괄호, Time: 9.85 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,18 +1,14 @@
 #스택 이용
-#( 이면 스택에 넣고, )이면 스택에서 팝하기, 만약에 팝하려고 하는데 스택 길이가 0이면 false 리턴하기
-#전체 순회를 하면 true 리턴하기
-
+#스택 한 곳에 전책 저장, 다른 한 곳엔 )을 저장하는데, 만약 순회를 돌다가 (이 나왔을 때 )들의 len 이 0이면 false리턴 전체 순회를 했을 시 true리턴
 def solution(s):
-    stack = []
-    for i in s:
-        if(i == '('):
-            stack.append('(')
+    stackB = [] #)
+    for s in list(s)[::-1]:
+        if s == ')':
+            stackB.append(')')
         else:
-            if(len(stack)>0):
-                stack.pop()
+            if len(stackB):
+                stackB.pop()
             else:
                 return False
-    if(len(stack)==0):
-        return True
-    else :
-        return False
+    return True if len(stackB)==0 else False
+                
