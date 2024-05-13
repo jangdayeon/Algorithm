9a9ab74b21c2f78b665c4c547512279b54d211db
ELOISE <dayeonjang.dev@gmail.com>
2024-05-13 17:38:13 +0900

    [level 2] Title: 멀리 뛰기, Time: 120.78 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,12 +1,12 @@
-#dp를 이용한 답 찾아봄
-def solution(n):
-    if n < 3 :
-        return n
-    li = [0] * (n+1)
-    li[1] = 1
-    li[2] = 2
-    for i in range(3,n+1):
-        li[i] = li[i-2]+li[i-1]
-        # (li[i-2] == li[i-2]에 2만큼 더 간 경우의 합)
-        # (li[i-1] == li[i-1]에 1만큼 더 간 경우의 합)
-    return li[n]%1234567
+#옛날에 이코테에서 바닥공사 문제랑 비슷함
+#dp로 풀어야할 듯
+
+#첫번째 틀림. for문을 돌리는데 min2Jump값이 변경되도록 코드를 짯음;
+import math
+def solution(num):
+    can2 = num//2
+    answer = 0
+    for i in range(can2+1):
+        nowLen = num-i
+        answer += math.factorial(nowLen)//(math.factorial(i)*math.factorial(nowLen-i))
+    return answer%1234567
