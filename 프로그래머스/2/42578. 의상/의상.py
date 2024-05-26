6c7a263e37bbc5cc00c42f41dc0d594901f8e36d
ELOISE <dayeonjang.dev@gmail.com>
2024-05-26 17:00:14 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,32 +1,15 @@
-from itertools import combinations 
+#조합 문제
+#안 입는 경우도 조건에 두어 계산 후 아무것도 안 입는 경우를 빼주기
 
-#category끼리 정렬해서 인덱스를 지정해주고, 해당 인덱스에 옷이 몇 개인지 clo에 저장해주기
 def solution(clothes):
-    category = list(set(list(map(lambda x:x[1], clothes))))
-    category = sorted(category)
-    clo = [0 for _ in range(len(category))]
-    for i in clothes:
-        clo[category.index(i[1])] +=1
-    # print(*category)
-    # print(*clo)
-    
-    
-    ###############이렇게 조합을 구해 계산하면 시간 초과 나옴##############
-    ########해결방법 옷 카테고리가 2가지일 경우 (N+1)(M+1)-1임. 한 옷을 입지 않는 경우의 수(+1)을 더하고, 모두 안 입을 경우의 수(-1)을 통해 계산 ##########################
-    # forCombi = 1
-    # for i in clo:
-    #     forCombi *=i
-    # answer = sum(clo) #한 개 고를 경우
-    # #이 아랜 두 개 이상 고를 경우
-    # for i in range(2,len(clo)+1): # i_C_j
-    #     a = combinations(clo,i) #combinations함수로 조합을 구하고
-    #     for j in a: #조합별로 수를 다 곱해 answer에 더하기
-    #         b = list(j) 
-    #         c = 1
-    #         for k in b :
-    #             c *=k
-    #         answer +=c
-        answer = 1
-        for i in clo:
-            answer *= (i+1)
-    return answer-1
+    closet = dict()
+    for c in clothes: #분류별로 옷 나누기
+        if c[1] in closet:
+            closet[c[1]] +=1
+        else :
+            closet[c[1]] = 1
+
+    result = 1
+    for c in closet: #조합 계산하기
+        result *= closet[c]+1
+    return result-1
