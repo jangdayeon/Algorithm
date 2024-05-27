3bc6be12c25d3f2ab8e8a9d7d06861f793811928
ELOISE <dayeonjang.dev@gmail.com>
2024-05-27 16:38:50 +0900

    [level 2] Title: 피로도, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,23 +1,18 @@
-#첫 번째 아이디어
-#모든 순서 경우의 수를 구하고, -> PERMUTATION LIB이용
-#해당 순서가 저장된 배열을 WHILE문을 돌리고
-#던전에서 활동하는 FOR문 돌리고
-#던전 몇 개 갈 수 있는지 최대값을 계속 구해서 리턴하기
+#소모 피로도가 작은 순으로 정렬해서 하려고 했지만,, 예외 발생 가능성 있어 완전 탐색해야 함
 
 from itertools import permutations
 def solution(k, dungeons):
-    order = list(map(list,permutations(range(len(dungeons)),len(dungeons))))
-    answer = -1
-    for i in order: #i = [0,1,2],[0,2,1] 이런 인덱스 순서
-        first = k
-        count = 0
-        for r,j in enumerate(i):
-            if(dungeons[j][0]>first):
-                answer = max(answer,count)
+    permu = permutations(range(0,len(dungeons)))
+    result = 0
+    for p in permu:
+        now_tired = k
+        if result == len(dungeons): #다 탐색 가능한 경우를 찾을 경우 탐색 종료하여 효율성 높임
+            break
+        for i, pp in enumerate(p): #탐색
+            if now_tired < dungeons[pp][0] :
                 break
-            else:
-                first = first - dungeons[j][1]
-                count +=1
-            if(r == len(i)-1):
-                return len(i)
-    return answer
+            else :
+                now_tired -= dungeons[pp][1]
+                result = max(result, i+1)
+    return result
+                
