4d66ce20a56c28abae2fde2856c13af01200f6dd
ELOISE <dayeonjang.dev@gmail.com>
2024-06-01 09:02:40 +0900

    [level 2] Title: 모음 사전, Time: 0.82 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,29 +1,18 @@
-#첫 번째 아이디어
-#a,e,i,o,u 각각 5개씩 있는 리스트를 하나 만들고
-#이 리스트의 combinations를 구하기
-#string으로서 정렬하기
-#word는 find 함수로 인덱스 구한 다음 인덱스 +1 리턴하기
+#경우를 다 구한다음 sort함수 이용
 
-from itertools import permutations
+from itertools import product
 def solution(word):
-    words = ['A','E','I','O','U']*5
-    combi = ['A','E','I','O','U']
-    a = set(permutations(words,2))
-    a = list(map(list,a))
-    a = list(map(lambda x:"".join(x), a))
-    combi += a
-    a = set(permutations(words,3))
-    a = list(map(list,a))
-    a = list(map(lambda x:"".join(x), a))
-    combi += a
-    a = set(permutations(words,4))
-    a = list(map(list,a))
-    a = list(map(lambda x:"".join(x), a))
-    combi += a
-    a = set(permutations(words,5))
-    a = list(map(list,a))
-    a = list(map(lambda x:"".join(x), a))
-    combi += a
-    combi.sort()
-    answer = combi.index(word)
-    return answer+1
+    mo = ['A','E','I','O','U']
+    per2 = map(lambda x:''.join(x), product(mo, repeat = 2))
+    per3 = map(lambda x:''.join(x), product(mo, repeat = 3))
+    per4 = map(lambda x:''.join(x), product(mo, repeat = 4))
+    per5 = map(lambda x:''.join(x), product(mo, repeat = 5))
+    mo += per2
+    mo += per3
+    mo += per4
+    mo += per5
+    mo.sort()
+    for i,m in enumerate(mo):
+        if m == word :
+            return i+1
+    return -1
