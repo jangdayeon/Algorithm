7956c1a5132feaad9ca54f751dfc840d363ec3ae
ELOISE <dayeonjang.dev@gmail.com>
2024-06-14 14:27:44 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,27 +1,17 @@
-#아이디어 바로 생각이 안났으니 한 번 더!
-
-#bridge의 길이가 0이 될 때까지 while문을 돌려두고
-#기본적으로, 다리를 건너는 트럭에서 하나의 값을 pop하기
-#그 다음에는,,
-#다음 들어올 차가 더해진 무게와 길이가 괜찮으면 대기 트럭에서 빼오고,
-#안 괜찮으면 0을 추가하기
-
 from collections import deque
+
 def solution(bridge_length, weight, truck_weights):
-    bridge = deque([0]* bridge_length)
-    answer = 0
-    current_sum = 0
-    while (len(bridge)!= 0):
-        # print(*bridge)
-        a = bridge.popleft()
-        answer +=1
-        current_sum -=a
-        if(len(truck_weights)!=0):
-            if(current_sum+truck_weights[0]>weight):
-                bridge.append(0)
-            else:
-                b = truck_weights.pop(0)
-                bridge.append(b)
-                current_sum +=b
-                
-    return answer
+    bridge = deque([0]*bridge_length)
+    i = 0
+    bridge_weight = 0
+    while bridge:
+        i+=1
+        bridge_weight -= bridge.popleft() #삭제를 먼저했어야 함. 삭제를 추가뒤에 했더니 계속 틀렸음
+        if truck_weights :
+            if (bridge_weight + truck_weights[0]) <= weight:
+                addTruck = truck_weights.pop(0)
+                bridge.append(addTruck)
+                bridge_weight += addTruck
+            else :
+                bridge.append(0) #이걸 수정함
+    return i
