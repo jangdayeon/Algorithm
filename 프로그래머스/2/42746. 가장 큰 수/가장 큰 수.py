e38ad8c4230e877e27e5df721fd235a162a402ca
ELOISE <dayeonjang.dev@gmail.com>
2024-06-14 12:22:17 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,8 +1,14 @@
+#str으로 바꿔 정렬해야한다는 건 알았는데, 30이랑 3이랑 비교할 때 어떻게 비교하면 좋을지 생각이 안나 답을 찾아봤다.
+#숫자가 30이면 303030, 3이면 333처럼 max_length만큼 반복해서 문자열을 만들어 비교하면 됐다.
+#테케 11번만 틀려서 확인했더니 [0,0]일 경우 0을 리턴해야 한다.
 def solution(numbers):
-    numbers = map(str, numbers)
-    a = list(map(lambda x:x*3, numbers))
-    a.sort(reverse=True)
-    a = list(map(lambda x:x[0:len(x)//3], a))
-    
-    answer = str((int(''.join(a))))
-    return answer
+    n = list(map(str, numbers))
+    max_length = max(map(len, n))
+    n = list(map(lambda x:x*max_length, n))
+    n.sort(reverse = True)
+    n = list(map(lambda x:x[:len(x)//max_length], n))
+    result = "".join(n)
+    if int(result) == 0 :
+        return '0'
+    else : 
+        return result 
