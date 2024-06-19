f0df0b9a244cd436ba2c65afd7147377d188bf0b
ELOISE <dayeonjang.dev@gmail.com>
2024-06-19 23:42:39 +0900

    [level 2] Title: 올바른 괄호, Time: 6.60 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=python3) 
 
 ### 성능 요약
 
-메모리: 11.6 MB, 시간: 9.85 ms
+메모리: 10.3 MB, 시간: 6.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 06일 11:12:51
+2024년 06월 19일 23:42:36
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,14 +1,10 @@
-#스택 이용
-#스택 한 곳에 전책 저장, 다른 한 곳엔 )을 저장하는데, 만약 순회를 돌다가 (이 나왔을 때 )들의 len 이 0이면 false리턴 전체 순회를 했을 시 true리턴
 def solution(s):
-    stackB = [] #)
-    for s in list(s)[::-1]:
-        if s == ')':
-            stackB.append(')')
-        else:
-            if len(stackB):
-                stackB.pop()
-            else:
-                return False
-    return True if len(stackB)==0 else False
-                
+    stack = []
+    for ss in s:
+        if ss == "(":
+            stack.append(0)
+        elif stack:
+            stack.pop()
+        else :
+            return False
+    return False if stack else True
