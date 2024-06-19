bfc81cc590e7461c326cebcf33a48ae9d009e700
ELOISE <dayeonjang.dev@gmail.com>
2024-06-19 23:46:36 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.55 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,7 +1,7 @@
 def solution(A,B):
-    A.sort(reverse=True) #작은수와 큰수가 곱해지기 위해 하나는 오름차순 하나는 내림차순으로 정렬
+    answer = 0
+    A.sort()
     B.sort()
-    result = 0
-    for a,b in zip(A,B):
-        result += a*b #곱한 값 누적해서 더한다음 리턴
-    return result
+    for i in range(len(A)):
+        answer += A[i]*B[len(A)-1-i]
+    return answer
