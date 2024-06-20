3a7795f76259b0850723ee82d6b33913889f6b35
ELOISE <dayeonjang.dev@gmail.com>
2024-06-20 23:43:54 +0900

    [level 2] Title: 숫자의 표현, Time: 3.50 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,13 +1,15 @@
 def solution(n):
-    result = 0
-    for i in range(1,n+1):
-        tmp = i
-        for j in range(i+1,n+1):
-            if tmp == n :
-                result +=1
-                break
-            elif tmp >n :
-                break
-            else:
-                tmp+=j
-    return result+1
+    li = [i for i in range(0,n+1)]
+    for i in range(1,len(li)):
+        li[i] += li[i-1]
+    l = 0
+    r = 1
+    cnt = 0
+    while l <= n and r <= n :
+        if li[r] - li[l] < n :
+            r += 1
+        else :
+            if li[r] - li[l] == n :
+                cnt += 1
+            l += 1
+    return cnt
