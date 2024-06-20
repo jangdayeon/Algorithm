96c401b3e15ade17828bf3afda9e8dd6588137f7
ELOISE <dayeonjang.dev@gmail.com>
2024-06-20 23:36:19 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 2.88 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,3 +1,8 @@
 def solution(s):
-    answer = []
-    return answer
+    result = [0,0] 
+    while s != '1':
+        result[1] += s.count('0')
+        result[0] += 1
+        s = "".join(s.split('0'))
+        s = format(int(len(s)),'b')
+    return result
