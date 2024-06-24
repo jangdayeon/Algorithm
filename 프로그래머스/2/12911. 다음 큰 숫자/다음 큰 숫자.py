1a3d29e5549ff445e5fa96d859d87b4fab2a133c
ELOISE <dayeonjang.dev@gmail.com>
2024-06-24 11:31:37 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,6 +1,6 @@
 def solution(n):
-    nCount1 = format(n,'b').count('1')
-    for i in range(n+1,1000001):
-        a = format(i,'b')
-        if str(a).count('1') == nCount1:
-            return i
+    result = n+1
+    one_count = format(n,'b').count('1')
+    while format(result,'b').count('1') != one_count :
+        result +=1
+    return result
