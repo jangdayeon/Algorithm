d8a3439a3672871f484624ab14aa31638d198eec
ELOISE <dayeonjang.dev@gmail.com>
2024-06-24 11:43:30 +0900

    [level 2] Title: 피보나치 수, Time: 96.92 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,9 +1,10 @@
 def solution(n):
-    f0 = 0
-    f1 = 1
+    a = 0
+    b = 1
     for i in range(2,n+1):
         if i%2==0:
-            f0 += f1
-        else :
-            f1 += f0
-    return f0%1234567 if n%2==0 else f1%1234567
+            a = (a+b)%1234567
+        else:
+            b = (b+a)%1234567
+    return a%1234567 if n%2==0 else b%1234567
+    
