b376915cace773f65072b6d1115b6dff5a65c9b6
ELOISE <dayeonjang.dev@gmail.com>
2024-06-25 13:07:16 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,12 +1,9 @@
-#옛날에 이코테에서 바닥공사 문제랑 비슷함
-#dp로 풀어야할 듯
-
-#첫번째 틀림. for문을 돌리는데 min2Jump값이 변경되도록 코드를 짯음;
-import math
-def solution(num):
-    can2 = num//2
-    answer = 0
-    for i in range(can2+1):
-        nowLen = num-i
-        answer += math.factorial(nowLen)//(math.factorial(i)*math.factorial(nowLen-i))
-    return answer%1234567
+def solution(n):
+    a = 1
+    b = 2
+    if n < 3 :
+        if n == 1 : return 1
+        else : return 2
+    for i in range(3,n+1):
+        a,b = b,a+b
+    return b%1234567
