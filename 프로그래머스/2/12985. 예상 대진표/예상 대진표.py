86d7fc39357ef8abc7447e461bfaad547761a104
ELOISE <dayeonjang.dev@gmail.com>
2024-06-25 12:54:48 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,19 +1,14 @@
-#짝수이면 자기 앞 참가자와, 홀수이면 자기 뒤 참가자와 붙음
-#한 번 틀림. if abs(a-b) == 1 이면 리턴하도록 했는데 그럼 3,4일 경우가 문제가 됨
-#두 번 틀림. a와 b 중 작은 수는 홀수, 큰 수는 짝수여야 함
+# 8,4,6 -> 3 인 것에서 예외 발생
+#다르게 풀어야 함
+
 def solution(n,a,b):
-    answer = 0
+    mini, maxi = (b,a) if a> b else (a,b)
+    answer = 1
     while True:
-        answer += 1
-        if abs(a-b) == 1 and (min(a,b)%2==1 and max(a,b)%2==0):
+        mini = mini//2 if mini%2==0 else (mini+1)//2
+        maxi = maxi//2 if maxi%2==0 else (maxi+1)//2
+        if mini == maxi :
             return answer
-        if a % 2 == 0 :
-            a = a/2
-        else :
-            a = (a+1)/2
-        if b % 2 == 0 :
-            b = b/2
-        else :
-            b = (b+1)/2
-        print(a,b)
-    return -1
+        answer+=1
+    return answer
+    
