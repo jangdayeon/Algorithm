68b485aa05cf0f48ab31c60f995d303f038e9d5d
ELOISE <dayeonjang.dev@gmail.com>
2024-06-26 14:07:50 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,11 +1,5 @@
-# 1. brown + yellow 한 값의 약수 짝을 구하고,
-# 2. 공약수를 a와 b라고 했을 때 (a+b) * 2 - 4 == brown인지 확인하고 만약 맞다면 그대로 리턴하기
 def solution(brown, yellow):
-    by = brown + yellow
-    checkList = []
-    for i in range(1,int(by**(1/2))+1):
-        if by % i == 0 :
-            checkList.append((i,by//i))
-    for a,b in checkList:
-        if (a+b)*2-4 == brown :
-            return [b,a]
+    for i in range(3,(brown+yellow)//2+1):
+        if (brown+yellow) % i == 0 and (i*2)+(((brown+yellow)//i)-2)*2 == brown:
+            return [((brown+yellow)//i), i];
+    return -1
