0f4fb4cd2ef8c1c86150a1aabad35359ec972065
ELOISE <dayeonjang.dev@gmail.com>
2024-06-26 15:26:01 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,9 +1,7 @@
-import math
+from math import gcd
+
 def solution(arr):
-    if len(arr) == 1 :
-        return arr[0]
-    else:
-        result = arr[0] * arr[1] // math.gcd(arr[0],arr[1])
-        for i in range(2,len(arr)):
-            result = result * arr[i] // math.gcd(result, arr[i])
-        return result
+    result = arr[0]
+    for i in range(1,len(arr)):
+        result = (result*arr[i]) // gcd(result,arr[i]) 
+    return result
