e01590d57c03d621e5fc722cbcfc85ab660acb4f
ELOISE <dayeonjang.dev@gmail.com>
2024-06-27 13:06:24 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,16 +1,10 @@
+import math
 def solution(n, words):
-    done = []
-    idx = 0
-    result = [0,0]
-    check = words[0][0]
-    for i,w in enumerate(words):
-        who = (i+1)%n if (i+1)%n !=0 else n
-        if done.count(w)>0 or w[0] != check:
-            result[0] = who
-            result[1] = (i+1)//n+1 if (i+1)%n != 0 else (i+1)//n 
-            break
-        done.append(w)
-        check = w[-1]
-    return result
-            
-        
+    turn = 0
+    done = set()
+    done.add(words[0])
+    for i in range(1,len(words)):
+        if words[i-1][-1] != words[i][0] or (words[i] in done):
+            return [(i+1)%n if (i+1)%n !=0 else n, math.ceil((i+1)/n)]
+        done.add(words[i])
+    return [0,0]
