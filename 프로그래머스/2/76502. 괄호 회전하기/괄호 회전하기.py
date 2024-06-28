5821cd50e53a5035f4638a2da1d793fc7f879dc7
ELOISE <dayeonjang.dev@gmail.com>
2024-06-28 13:48:15 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,27 +1,27 @@
-#stack 이용하여 올바른 괄호 문자열인지 확인
+from collections import deque
 
 def solution(s):
-    stack = [] #짝을 넣고 순회를 끝냈는데 stack에 원소가 남아있으면 false 아니면 true
+    s = deque(s)
     result = 0
-    check_stack = True
     for i in range(len(s)):
-        s = s[1:]+s[0]
-        for ss in s[::-1]:
-            if stack and ss =='{' and stack[-1]=='}':
-                stack.pop()
-            elif stack and ss =='[' and stack[-1]==']':
-                stack.pop()
-            elif stack and ss =='(' and stack[-1]==')':
-                stack.pop()
-            elif ss in ['}',']',')']:
+        stack = []
+        t = s.popleft()
+        s.append(t)
+        allVisit = True
+        for ss in list(s):
+            if ss in ['[','(','{']:
                 stack.append(ss)
-            else :
-                check_stack = False
-                break
-        if check_stack and len(stack) == 0 :
-            result += 1  
-        else:
-            stack = []
-        check_stack = True
+            else:
+                if ss == ']' and stack and stack[-1] == '[':
+                    stack.pop()
+                elif ss == ')' and stack and stack[-1] == '(':
+                    stack.pop()
+                elif ss == '}' and stack and stack[-1] == '{':
+                    stack.pop()
+                else:
+                    allVisit = False
+                    break
+        if not stack and allVisit:
+            result += 1
     return result
-        
+            
