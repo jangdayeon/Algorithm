260a9ff9a6706252cfd3547b89388e4bb746e94c
ELOISE <dayeonjang.dev@gmail.com>
2024-06-28 14:21:28 +0900

    [level 2] Title: 할인 행사, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,24 +1,18 @@
-#1. want랑 number을 zip하여 dict으로 만들고,
-#2. discount를 10개씩 묶고 만약 counter을 통해 개수를 구한다음
-#모든 원소가 1 <= 2 이면 result += 1을 하도록 코드를 짤 예정 
 from collections import Counter
+
 def solution(want, number, discount):
-    
-    # 1.
-    want_dic = {}
-    for i in range(len(want)):
-        want_dic[want[i]] = number[i]
-        
+    want_cnt = sum(number)
+    want_cntr = dict(zip(want,number))
     result = 0
-    # 2.
-    for i in range(0, len(discount)-9):
-        dc_c = Counter(discount[i:i+10])
-        canIBuyAll = True
-        for w in want_dic:
-            if w not in dc_c or want_dic[w] > dc_c[w]:
-                canIBuyAll = False
+    for i in range(want_cnt-1,len(discount)):
+        cntr = Counter(discount[i-want_cnt+1:i+1])
+        cntr.subtract(want_cntr)
+        canBuy = True
+        for _cntr in cntr:
+            if cntr[_cntr] < 0 :
+                canBuy = False
                 break
-        if canIBuyAll :
-            result += 1
-    
+        if canBuy:
+            result +=1
     return result
+                
