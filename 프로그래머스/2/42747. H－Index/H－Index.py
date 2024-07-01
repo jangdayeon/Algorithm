71ad5175fb8d03d5a056a4219da0224a0331a517
ELOISE <dayeonjang.dev@gmail.com>
2024-07-01 15:08:07 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,10 +1,8 @@
 def solution(citations):
-    for i in range(max(citations),-1,-1):
-        up_down = [0,0]
+    for ch in range(max(citations),-1,-1): #ch = 인용 횟수
+        more = 0 #h번 이상 인용된 논문
         for c in citations:
-            if c >= i :
-                up_down[0] += 1
-            else:
-                up_down[1] += 1
-        if up_down[0] >= i and up_down[1] <= i :
-            return i
+            if c>=ch:
+                more += 1
+        if more >= ch and len(citations)-more <= ch:
+            return ch
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,6 +1,6 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 24일 14:09:02
+2024년 07월 01일 15:08:05
 
 ### 문제 설명
 
