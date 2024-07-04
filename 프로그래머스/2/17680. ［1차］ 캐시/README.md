b859f62995a90585c4649ebaea28ea9a6ab81c3a
ELOISE <dayeonjang.dev@gmail.com>
2024-07-04 14:29:32 +0900

    [level 2] Title: [1차] 캐시, Time: 1.39 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.76 ms
+메모리: 10.4 MB, 시간: 1.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 25일 00:36:56
+2024년 07월 04일 14:29:28
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,20 +1,24 @@
-#한 번 틀림. 답 확인. cacheSize가 0일 때 deque에 값을 넣는 문제가 발생함
 from collections import deque
+
 def solution(cacheSize, cities):
     if cacheSize == 0:
-        return 5*len(cities)
-    cities = map(lambda x:x.lower(), cities) #소문자로 수정
-    cacheList = deque([""] * cacheSize) #캐시 담을 곳
+        return len(cities)*5
+    cities = map(lambda x:x.upper(), cities)
+    cache = deque([])
     result = 0
-    for c in cities: 
-        if c in cacheList: #캐시에 저장된 값이면
-            cacheList.remove(c)
-            cacheList.append(c)
-            result += 1
-        else : #저장되지 않은 값이면
-            if cacheList :
-                cacheList.popleft()
-            cacheList.append(c)
+    for city in cities:
+        if len(cache) == 0:
+            cache.append(city)
             result += 5
+            continue
+        if city not in cache:
+            if len(cache) == cacheSize:
+                cache.popleft()
+            cache.append(city)
+            result += 5
+        else :
+            cache.remove(city)
+            cache.append(city)
+            result += 1
     return result
-    
+            
