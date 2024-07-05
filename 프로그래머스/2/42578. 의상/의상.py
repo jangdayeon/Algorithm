e0f172a2d26ae7549a595419efd7cfc01ab460ae
ELOISE <dayeonjang.dev@gmail.com>
2024-07-05 13:44:03 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,15 +1,11 @@
-#조합 문제
-#안 입는 경우도 조건에 두어 계산 후 아무것도 안 입는 경우를 빼주기
-
 def solution(clothes):
-    closet = dict()
-    for c in clothes: #분류별로 옷 나누기
-        if c[1] in closet:
-            closet[c[1]] +=1
-        else :
-            closet[c[1]] = 1
-
+    dic = {}
+    for c in clothes:
+        if c[1] in dic:
+            dic[c[1]]+=1
+        else:
+            dic[c[1]] =1           
     result = 1
-    for c in closet: #조합 계산하기
-        result *= closet[c]+1
-    return result-1
+    for d in dic.values():
+        result *=(d+1)
+    return result -1
