ba715c4f6d3e7d79154a3d9ffee70a1934a9f0de
ELOISE <dayeonjang.dev@gmail.com>
2024-07-08 18:25:40 +0900

    [level 2] Title: 피로도, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,18 +1,14 @@
-#소모 피로도가 작은 순으로 정렬해서 하려고 했지만,, 예외 발생 가능성 있어 완전 탐색해야 함
-
 from itertools import permutations
 def solution(k, dungeons):
-    permu = permutations(range(0,len(dungeons)))
+    combi = permutations(range(len(dungeons)))
     result = 0
-    for p in permu:
-        now_tired = k
-        if result == len(dungeons): #다 탐색 가능한 경우를 찾을 경우 탐색 종료하여 효율성 높임
-            break
-        for i, pp in enumerate(p): #탐색
-            if now_tired < dungeons[pp][0] :
+    for c in combi:
+        now = k
+        for i,cc in enumerate(c):
+            least, lose = dungeons[cc][0], dungeons[cc][1]
+            if now >= least:
+                now -= lose
+                result = i+1 if i+1>result else result
+            else:
                 break
-            else :
-                now_tired -= dungeons[pp][1]
-                result = max(result, i+1)
     return result
-                
