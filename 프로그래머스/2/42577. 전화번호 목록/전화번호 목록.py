6d6c8246f46c94c1d0480c50c523d4d1bae0a036
ELOISE <dayeonjang.dev@gmail.com>
2024-07-15 10:42:18 +0900

    [level 2] Title: 전화번호 목록, Time: 99.35 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,6 @@
-#phone_book의 길이가 백만이므로 1중 for문만으로 코드를 짜야 함
-#string으로 정렬하면 앞글자가 같은 것부터 정렬되니깐 그 점을 이용해 나랑 나의 뒷값을 비교했다.
 def solution(phone_book):
     phone_book.sort()
-
-    for i in range(len(phone_book)-1):
+    for i in range(0,len(phone_book)-1):
         if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
             return False
     return True
