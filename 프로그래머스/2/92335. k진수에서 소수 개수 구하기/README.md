6aa682b11f83678e6bddf07e1a1f853d0e9feb57
ELOISE <dayeonjang.dev@gmail.com>
2024-07-17 16:38:12 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 117.22 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.04 ms
+메모리: 10.5 MB, 시간: 117.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 31일 18:49:00
+2024년 07월 17일 16:38:12
 
 ### 문제 설명
 
--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,55 +1,27 @@
-#많이 틀림. 소수 판별에서 아리토스테네스의 체를 이용했는데, k 이하의 모든 소수를 구하는 방식이었어서 적절치 않았음
-# 제곱근을 이용한 소수 판별로 문제를 해결하면 된다는 것을 알게됐다.
-# 추가로 continue랑 break,, 이중 반복문 기준으로 생각하지 말고 1중으로만 생각해라
 def solution(n, k):
-    muck = 0 #몫
-    rest = "" #나머지
-    if k == 10:
-        rest = str(int(n))
-    else:
-        while True: #진수 변환
-            rest += str(n % k)
-            n = n // k
-            if n < 1 :
-                break
-        rest = rest[::-1]
-    #여기부터는 0을 기준으로 문자열 나누기
-    rest = rest.split('0')
-    
-    for r in range(rest.count('')):
-        rest.remove('')
-    print(rest)
-    rest = list(map(int, rest))
-    maxi = max(rest)
-    
-    # #소수 구하기(에라토스테네스의 체)
-    # sosu = [True] * (maxi+1)
-    # sosu[0], sosu[1] = True,False
-    # for i in range(2, maxi+1):
-    #     if sosu[i]:
-    #         j = 2 
-    #         while j*i < maxi+1:
-    #             sosu[j*i] = False
-    #             j += 1
-    #소수인 값 몇 갠지 구하기
-    # result = 0
-    # for r in rest:
-    #     if sosu[r] :
-    #         result += 1
-    # return result
-    
+    change = []
+    while n > 0 :
+        div, mod = divmod(n,k)
+        change.append(int(mod))
+        n = div
+    change.reverse()
     result = 0
-    #소수 구하기(제곱근)
-    for i in rest:
-        sosu = True
-        if i <= 1: 
-            pass
-        else:
-            for j in range(2, int(i**0.5)+1):
-                if i % j == 0:
-                    sosu = False
-                    break
-            if sosu:
-                result += 1
+    start = 0
+    for i,c in enumerate(change):
+        if c == 0 :
+            if start != i :
+                result += checkSosu(change[start:i])
+            start = i+1
+        if i == len(change)-1 and start != i+1:
+            result += checkSosu(change[start:])
     return result
-    
+
+def checkSosu(n):
+    n = int("".join(map(lambda x:str(x), n)))
+    print(n)
+    if n == 1 :
+        return 0
+    for i in range(2,int(n**(1/2))+1):
+        if n % i == 0 :
+            return 0
+    return 1
