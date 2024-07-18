d68cc8b7ce7cd8953733e03d1cca672e82c6f885
ELOISE <dayeonjang.dev@gmail.com>
2024-07-18 10:56:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.86 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,20 +1,17 @@
-#bfs 구현 문제 / 통로 다 구한 다음에 최솟값 리턴해야 함
 from collections import deque
 def solution(maps):
-    direction = [(-1,0),(1,0),(0,-1),(0,1)] #상,하.좌,우
-    queue = deque([(0,0)])
-    n, m = len(maps), len(maps[0])
-    now_distance = 0
-    while queue: #bfs로 maps에서 최소값 업데이트
-        now = queue.popleft()
-        for d in direction:
-            a, b = d[0],d[1]
-            if 0 <= now[0]+a < n and 0 <= now[1]+b < m :
-                if maps[now[0]+a][now[1]+b] == 1 :
-                    maps[now[0]+a][now[1]+b] = maps[now[0]][now[1]] + 1
-                    queue.append((now[0]+a,now[1]+b))
-    
-    if maps[n-1][m-1] == 1: #접근 못한 경우 ( 제한 사항에서 n ==m == 1 인 경우 제외한다고 하였기에)
-        return -1
-    else: #접근 한 경우
-        return maps[n-1][m-1]
+    q = deque([(0,0)])
+    visited = [[-1] * len(maps[0]) for _ in range(len(maps))]
+    visited[0][0] = 1
+    dirt = [(-1,0),(1,0),(0,-1),(0,1)] #상하좌우
+    while q:
+        n_i,n_j = q.popleft()
+        for (d_i, d_j) in dirt:
+            if -1<n_i+d_i<len(maps) and -1<n_j+d_j<len(maps[0]) and visited[n_i+d_i][n_j+d_j] == -1 and maps [n_i+d_i][n_j+d_j] == 1 :
+                visited[n_i+d_i][n_j+d_j] = visited[n_i][n_j] + 1
+                q.append((n_i+d_i,n_j+d_j))
+                if n_i+d_i == len(maps)-1 and n_j+d_j == len(maps[0])-1 :
+                    return visited[len(maps)-1][len(maps[0])-1]
+    return -1
+            
+                
