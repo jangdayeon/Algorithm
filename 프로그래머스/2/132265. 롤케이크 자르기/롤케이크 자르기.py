0b3e794056315fa117e44b6bfbe575bc9ab852a0
ELOISE <dayeonjang.dev@gmail.com>
2024-07-29 20:09:45 +0900

    [level 2] Title: 롤케이크 자르기, Time: 673.28 ms, Memory: 50.5 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,22 +1,22 @@
-#set함수를 이용해서 len을 비교하면 될 것 같음
-#for문은 한 번만
-#한 번 틀림. 네.. 시간 초과입니다.
-#세 번 틀림. set()함수 만드는 것은 O(N)인 것 같다. 대신 add는 O(1)
-#네 번 틀림. 방법을 a: 앞->뒤 b: 뒤->앞으로 해서 개수를 모두 구한 다음 마지막에 a,b를 zip해서 개수가 같은 걸 찾는다!
+from collections import Counter
 def solution(topping):
+    left = Counter(topping)
+    left_cnt = len(left)
+    right = Counter()
+    right_cnt = 0
     result = 0
-    kinds = len(set(topping))
-    a_kinds = [0]
-    b_kinds = [0]
-    a = set()
-    b = set()
-    for i in range(len(topping)):
-        a.add(topping[i])
-        b.add(topping[len(topping)-1-i])
-        a_kinds.append(len(a))
-        b_kinds.append(len(b))
-    b_kinds = b_kinds[::-1]
-    for i in range(len(a_kinds)):
-        if a_kinds[i] == b_kinds[i]:
-            result += 1
+    for t in topping:
+        left[t] -= 1
+        if left[t] == 0 :
+            left_cnt -= 1
+        if t in right:
+            right[t] +=1
+        else:
+            right[t] = 1
+            right_cnt +=1
+        if left_cnt == right_cnt :
+            result +=1
     return result
+            
+        
+        
