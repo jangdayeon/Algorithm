285716883ff2f4d35529f479d86c0ae154a5ed28
ELOISE <dayeonjang.dev@gmail.com>
2024-07-31 18:52:10 +0900

    [level 2] Title: 주식가격, Time: 29.01 ms, Memory: 20.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,24 +1,12 @@
-#이것도 아이디어가 ,, ㅎㅎ prices 길이가 10만까지 가서 log2N으로 풀었을 줄 알았는데 n2으로도 가능
-#정석대로 풀었다.
-#첫번째값 이후에 첫번째 값보다 작은 값 발견시 해당 인덱스를 answer에 추가하고 for문을 break하고 다음 인덱스로 넘어가기
-
+#답 확인함
+#일단 stack의 맨 나중에 들어온 애가 현재값보다 크면 pop해줘서 중간에 엄청 큰 값이 있어도 상관이 없다.
 def solution(prices):
-    answer = []
-    for i in range(len(prices)):
-        a = prices[i]
-        if(i == len(prices)-1):
-            answer.append(0)
-        else:
-            b = 100000
-            whereB = -1
-            for j in range(i+1,len(prices)):
-                if(prices[j]<a):
-                    b = prices[j]
-                    whereB = j
-                    break
-            if (a>b):
-                answer.append(whereB-i)
-                # print(b, "index = ", whereB)
-            else:
-                answer.append(len(prices)-i-1)
+    stack = []
+    # answer을 max값으로 초기화  
+    answer = [ i for i in range (len(prices) - 1, -1, -1)]
+    for i,p in enumerate(prices):
+        while stack and prices[stack[-1]] > p :
+            j = stack.pop()
+            answer[j] = i-j
+        stack.append(i)
     return answer
