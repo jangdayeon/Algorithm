da8e66f6b36aa313b6b51ea117dc86eee9da3a5a
ELOISE <dayeonjang.dev@gmail.com>
2024-07-31 16:36:13 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,6 +1,8 @@
 def solution(word):
-    dic = {'A':0,'E':1, 'I':2,'O':3, 'U':4}
+    dic = {'A':1,'E':2, 'I':3,'O':4, 'U':5}
     result = 0
     for i,w in enumerate(word):
-        result += dic[w]*1*(5**(5-i)-1) / 4
-    return result + len(word)
+        # print(i,(5**i),(5**(5-i)-1))
+        # print((5**i)*(5**(5-i)-1)/4)
+        print(dic[w]*(5**(5-i)-1) / 4)
+        # print(result)
