0b877264d7d4e5fce62877ea21e6e3934b9d61c2
ELOISE <dayeonjang.dev@gmail.com>
2024-07-31 20:18:39 +0900

    [level 2] Title: 땅따먹기, Time: 166.10 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,9 +1,8 @@
-#dp 문제인 것 같다 이전값의 최대값을 현재값에 더해주는 것을 끝까지 반복했다.
+#dp 이용하기
 def solution(land):
     for i in range(1, len(land)):
         land[i][0] += max(land[i-1][1],land[i-1][2],land[i-1][3])
-        land[i][1] += max(land[i-1][0],land[i-1][2],land[i-1][3])
-        land[i][2] += max(land[i-1][1],land[i-1][0],land[i-1][3])
+        land[i][1] += max(land[i-1][0],land[i-1][2],land[i-1][3])        
+        land[i][2] += max(land[i-1][1],land[i-1][0],land[i-1][3])        
         land[i][3] += max(land[i-1][1],land[i-1][2],land[i-1][0])
-    return max(land[len(land)-1])
-        
+    return max(land[-1])
