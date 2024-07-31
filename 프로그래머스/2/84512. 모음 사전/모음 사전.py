31340961ee18a59d1c1f2128a9216744b19d730b
ELOISE <dayeonjang.dev@gmail.com>
2024-07-31 16:35:38 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,18 +1,6 @@
-#경우를 다 구한다음 sort함수 이용
-
-from itertools import product
 def solution(word):
-    mo = ['A','E','I','O','U']
-    per2 = map(lambda x:''.join(x), product(mo, repeat = 2))
-    per3 = map(lambda x:''.join(x), product(mo, repeat = 3))
-    per4 = map(lambda x:''.join(x), product(mo, repeat = 4))
-    per5 = map(lambda x:''.join(x), product(mo, repeat = 5))
-    mo += per2
-    mo += per3
-    mo += per4
-    mo += per5
-    mo.sort()
-    for i,m in enumerate(mo):
-        if m == word :
-            return i+1
-    return -1
+    dic = {'A':0,'E':1, 'I':2,'O':3, 'U':4}
+    result = 0
+    for i,w in enumerate(word):
+        result += dic[w]*1*(5**(5-i)-1) / 4
+    return result + len(word)
