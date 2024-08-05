bbca1b176edeab6f48916cc9974594108c7159ac
ELOISE <dayeonjang.dev@gmail.com>
2024-08-05 16:52:43 +0900

    [level 2] Title: 더 맵게, Time: 1866.81 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,15 +1,10 @@
-#우선순위 힙으로 구현
-#한 번 틀림. 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우를 고려하지 않아서
 import heapq
 def solution(scoville, K):
-    heapq.heapify(scoville) #힙으로 변환
-    result = 0
-    while scoville[0]<K:
+    heapq.heapify(scoville)
+    answer = 0
+    while scoville[0] < K :
         if len(scoville) < 2 :
             return -1
-        a = heapq.heappop(scoville)
-        b = heapq.heappop(scoville)
-        heapq.heappush(scoville, a+(b*2))
-        result +=1
-    return result
-    
+        heapq.heappush(scoville, (heapq.heappop(scoville) + heapq.heappop(scoville)*2))
+        answer += 1
+    return answer
