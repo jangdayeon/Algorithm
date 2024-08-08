f1845425184b74525e83c2a69fa2914951c8af2a
ELOISE <dayeonjang.dev@gmail.com>
2024-08-08 23:08:04 +0900

    [level 2] Title: 스킬트리, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/README.md
+++ 프로그래머스/2/49993. 스킬트리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 스킬트리 - 49993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.02 ms
+메모리: 10.1 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 07일 15:26:24
+2024년 08월 08일 23:08:01
 
 ### 문제 설명
 
--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,31 +1,24 @@
-#skill의 C,B,D를 skill_trees에 index를 구해 order =  []에 각 인덱스에 맞는 값을 넣고
-#만약 order에 있는 값보다 index가 더 낮으면 해당 값을 지워버린다.
-
-#첫번째 틀림. skill_trees에 해당 skill 없는 경우를 고려한다는 것을 깜빡함..
-#두번째 틀림. 테케 3,5,13 틀림. 
-    # -> 답 찾아봄
-    # skill = "CBD"
-    # skill_trees = ["CED"] 인 경우 답이 1이 나와서 생긴 문제였음..
 def solution(skill, skill_trees):
-    order = dict(zip(skill_trees,[ [-1,-1] for _ in range(len(skill_trees)) ] )) 
-    # "BACDE" : [마지막 skill, 마지막 skill의 skill_trees에서의 위치]
-    for j, s in enumerate(skill):
-        where_del = []
-        for i in range(len(skill_trees)):
-            idx = skill_trees[i].find(s) #위치 찾고
-            if idx == -1 : #skill이 없는 경우 고려
-                continue
-            if order[skill_trees[i]][1] == -1 and j>=1: #앞 스킬이 아예 안나왔는데 뒤 스킬 나올 경우
-                print(s, skill_trees[i])
-                where_del.append(i)
-                continue
-            if order[skill_trees[i]][1] > idx or order[skill_trees[i]][0]+1 < j : #위치가 이전 스킬보다 앞에 있다?
-                where_del.append(i) #이건 불가능한 스킬트립
-            else: #위치가 이전 스킬보다 뒤이다?
-                order[skill_trees[i]][1] = idx #아직까진 가능하니 order 인덱스 갱신
-                order[skill_trees[i]][0] = j
-        where_del.sort(reverse=True) #삭제할 인덱스를 내림차순 정렬해 뒤 인덱스부터 삭제 가능하도록 함
-        for w in where_del: #삭제 코드
-            skill_trees.pop(w)
-    return len(skill_trees)
+    answer=0
+    for st in skill_trees: # st = "BACDE"
+        before, flag = 0, 0
+        for s in skill :
+            tmp = st.find(s)
             
+            if tmp >= 0 : #st에 skill이 있을 경우
+                if before == -1 : #전 스킬이 없었음! 다음 스킬도 못함
+                    flag = 1
+                    break
+                if tmp >= before : #현재스킬이 전 스킬보다 뒤 -> 유효
+                    before = tmp
+                else : #현새스킬이 전 스킬보다 앞 -> X
+                    flag = 1
+                    break
+            else: #st에 skill에 없음
+                before = tmp
+        if flag: 
+            continue
+        
+        answer+=1
+    return answer
+        
