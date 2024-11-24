b5c50f6f50601274dbe10b6becc5ce0c3677229b
ELOISE <dayeonjang.dev@gmail.com>
2024-11-24 19:35:30 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 0.85 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -1,40 +1,27 @@
-import java.util.Arrays;
-
-
 class Solution {
     public int solution(int[] mats, String[][] park) {
-        Arrays.sort(mats);
-        
-        int result = -1; 
-
-        for (int i = 0; i < park.length; i++) {
-            for (int j = 0; j < park[i].length; j++) {
-                if (park[i][j].equals("-1")) {
-                    int width = checkWidth(mats,park, new int[]{i, j});
-                    result = result > width ? result : width ;
-}
-            }
-        }
-
-        return result; 
-    }
-    
-    private int checkWidth(int[] mats, String[][] park, int[] start){
-        for(int ii=mats.length-1;ii>-1;ii--) {
-            int m = mats[ii];
-            if ( start[0]+m > park.length || start[1]+m > park[0].length) continue;
-            boolean flag = false;
-            label:
-            for (int i = start[0]; i < start[0]+m ; i++) {
-                for (int j = start[1]; j < start[1]+m ; j++) {
-                    if (!park[i][j].equals("-1")) { 
-                        flag = true;
-                        continue label;
-                    }
+        int[][] n_park = new int[park.length][park[0].length];
+        for(int i=0;i<park.length;i++){
+            for(int j=0;j<park[0].length;j++){
+                if(park[i][j].equals("-1")){
+                    n_park[i][j] = 1;
                 }
             }
-            if (flag  != true ) return m;
         }
-        return -1;
+
+        int answer = -1;
+        for(int i=1;i<park.length;i++){
+            for(int j=1;j<park[0].length;j++){
+                if(n_park[i][j]==1 && (n_park[i-1][j-1]!=0 && n_park[i-1][j]!=0&&n_park[i][j-1]!=0)){
+                    n_park[i][j] += Math.min(n_park[i-1][j-1],Math.min(n_park[i-1][j],n_park[i][j-1]));
+                }
+                
+                for(int m : mats){
+                    if(m == n_park[i][j]) answer = Math.max(m,answer);
+                }
+            }
+        }
+        
+        return answer;
     }
 }
