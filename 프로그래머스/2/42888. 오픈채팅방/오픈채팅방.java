307f2d1bcc6326142b5976ba2f6bd6e91c2569bc
ELOISE <dayeonjang.dev@gmail.com>
2024-11-24 20:20:04 +0900

    [level 2] Title: 오픈채팅방, Time: 182.32 ms, Memory: 194 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,26 +1,23 @@
-import java.util.stream.Stream;
 import java.util.*;
+
 class Solution {
     public String[] solution(String[] record) {
-        
-        String[][] splitR = new String[record.length][3];
-        Map<String, String> name = new HashMap<>();
-        for(int i=0; i<record.length;i++){
-            splitR[i] = record[i].split(" ");
-            if(!splitR[i][0].equals("Leave")){
-                name.put(splitR[i][1],splitR[i][2]);
+        Map<String,String> m = new HashMap<>();
+        for(int i=0;i<record.length;i++){
+            String[] tmp = record[i].split(" ");
+            if(!tmp[0].equals("Leave")) {
+                m.put(tmp[1],tmp[2]);
             }
         }
-        
         List<String> rtn = new ArrayList<>();
-        for(int i =0;i<splitR.length;i++){
-            switch(splitR[i][0]){
-                    case "Enter" -> rtn.add(name.get(splitR[i][1])+"님이 들어왔습니다.");
-                    case "Leave" -> rtn.add(name.get(splitR[i][1])+"님이 나갔습니다.");
-                    
+        for(int i=0;i<record.length;i++){
+            String[] tmp = record[i].split(" ");
+            switch(tmp[0]){
+                    case "Enter"->rtn.add(m.get(tmp[1])+"님이 들어왔습니다.");
+                    case "Leave"->rtn.add(m.get(tmp[1])+"님이 나갔습니다.");
             }
         }
         
-        return rtn.toArray(String[]::new);
+        return rtn.toArray(new String[0]);
     }
 }
