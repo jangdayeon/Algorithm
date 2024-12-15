2cd5eda9790c2fd20629272500ca239bf43259aa
ELOISE <dayeonjang.dev@gmail.com>
2024-12-15 12:45:14 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 113.40 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667?language=java) 
 
 ### 성능 요약
 
-메모리: 19.3 MB, 시간: 44.19 ms
+메모리: 136 MB, 시간: 113.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 19일 15:36:19
+2024년 12월 15일 12:45:13
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        int answer = 0;
+
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+
+        for (int num : queue1) q1.add(num);
+        for (int num : queue2) q2.add(num);
+
+        long s1 = q1.stream().mapToLong(Integer::longValue).sum();
+        long s2 = q2.stream().mapToLong(Integer::longValue).sum();
+
+        long total = s1 + s2;
+
+        if (total % 2 != 0) return -1; // 두 큐의 합이 홀수라면 균형 불가능
+
+        long target = total / 2;
+
+        while (s1 != s2) {
+            if (answer > (queue1.length + queue2.length) * 3) return -1;
+
+            if (s1 > s2) {
+                if (q1.isEmpty()) return -1;
+                int removeOne = q1.remove();
+                q2.add(removeOne);
+                s1 -= removeOne;
+                s2 += removeOne;
+            } else {
+                if (q2.isEmpty()) return -1;
+                int removeOne = q2.remove();
+                q1.add(removeOne);
+                s1 += removeOne;
+                s2 -= removeOne;
+            }
+
+            answer++;
+        }
+
+        return answer;
+    }
+}
